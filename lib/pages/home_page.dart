5df8edb02f8fb36cd737fe2020c9992ed97312c6
shameer-entspos <shameer.entspos@gmail.com>
2025-02-11 22:13:36 +0500

    Updated UI 03

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,34 +28,6 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Color(0xFF2176ff),
 
-      // bottomNavigationBar: BottomNavigationBar(
-      //   selectedItemColor: Colors.blue[600], // Color of the selected label
-      //   unselectedItemColor: Colors.grey, // Color of the unselected labels
-
-      //   backgroundColor: Colors.white,
-      //   items: [
-      //     BottomNavigationBarItem(
-      //       backgroundColor: Colors.white,
-      //       icon: Icon(Icons.home, color: Colors.blue[600], size: 30),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       backgroundColor: Colors.white,
-      //       icon: Icon(Icons.favorite, color: Colors.blue[600], size: 30),
-      //       label: 'Favourites',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       backgroundColor: Colors.white,
-      //       icon: Icon(Icons.chat, color: Colors.blue[600], size: 30),
-      //       label: 'Messages',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       backgroundColor: Colors.white,
-      //       icon: Icon(Icons.person, color: Colors.blue[600], size: 30),
-      //       label: 'Profile',
-      //     ),
-      //   ],
-      // ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
--- lib/utils/wrapper.dart
+++ lib/utils/wrapper.dart
@@ -10,6 +10,35 @@ class Wrapper extends StatefulWidget {
   State<Wrapper> createState() => _WrapperState();
 }
 
+// bottomNavigationBar: BottomNavigationBar(
+//   selectedItemColor: Colors.blue[600], // Color of the selected label
+//   unselectedItemColor: Colors.grey, // Color of the unselected labels
+
+//   backgroundColor: Colors.white,
+//   items: [
+//     BottomNavigationBarItem(
+//       backgroundColor: Colors.white,
+//       icon: Icon(Icons.home, color: Colors.blue[600], size: 30),
+//       label: 'Home',
+//     ),
+//     BottomNavigationBarItem(
+//       backgroundColor: Colors.white,
+//       icon: Icon(Icons.favorite, color: Colors.blue[600], size: 30),
+//       label: 'Favourites',
+//     ),
+//     BottomNavigationBarItem(
+//       backgroundColor: Colors.white,
+//       icon: Icon(Icons.chat, color: Colors.blue[600], size: 30),
+//       label: 'Messages',
+//     ),
+//     BottomNavigationBarItem(
+//       backgroundColor: Colors.white,
+//       icon: Icon(Icons.person, color: Colors.blue[600], size: 30),
+//       label: 'Profile',
+//     ),
+//   ],
+// ),
+
 class _WrapperState extends State<Wrapper> {
   @override
   Widget build(BuildContext context) {
